8208fc1f0be77a5bea192418a24ccacdc2f7ef57
Sarah Hayat <saraahyt@gmail.com>
2022-05-04 20:07:19 +0200

    meeting done

--- lib/pages/vote_page.dart
+++ lib/pages/vote_page.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:amoungirl/widgets/text_field_decoration.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import "package:socket_io_client/socket_io_client.dart" as IO;
 
 class VotePage extends StatefulWidget {
   static const routeName = 'vote';
@@ -12,39 +13,19 @@ class VotePage extends StatefulWidget {
 }
 
 class VotePageState extends State<VotePage> {
-  late Timer _timer;
-  int _start = 10;
+  late IO.Socket socket;
+
+  String left = "";
 
   @override
   void initState() {
-    startTimer();
+    onSocket();
     super.initState();
   }
 
   @override
   void dispose() {
     super.dispose();
-    _timer.cancel();
-  }
-
-  void startTimer() {
-    const oneSec = Duration(seconds: 1);
-    _timer = Timer.periodic(
-      oneSec,
-      (Timer timer) {
-        if (_start == 0) {
-          setState(() {
-            //TODO put put url false
-            print("timer done");
-            timer.cancel();
-          });
-        } else {
-          setState(() {
-            _start--;
-          });
-        }
-      },
-    );
   }
 
   @override
@@ -58,9 +39,34 @@ class VotePageState extends State<VotePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text("C'est la réu les gars, il est temps de tuer du boug"),
-              Text("$_start secondes", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+              Text(
+                "$left secondes",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
             ],
           ),
         ));
+  }
+
+  void onSocket() {
+    // socket = IO.io("https://amoung-irl-server-game.herokuapp.com/",
+    //     IO.OptionBuilder().setTransports(['websocket']).build());
+    socket = IO.io("http://192.168.1.18:3000",
+        IO.OptionBuilder().setTransports(['websocket']).build());
+    socket.connect();
+
+    socket.on('connect', (data) {
+      // socket.emit('startGame');
+      print("socket connect ${socket.connected}");
+    });
+
+    socket.on('meeting', (data) {
+      setState(() {
+        left = data['countDown'].toString();
+      });
+      if(data['countDown'] == 0){
+        Navigator.of(context).pop();
+      }
+    });
   }
 }
