05d7086d376cca7f027747f803ed4cb013d95636
Sarah Hayat <saraahyt@gmail.com>
2022-05-04 17:05:31 +0200

    Choose player and start game

--- lib/pages/game_config_page.dart
+++ lib/pages/game_config_page.dart
@@ -1,10 +1,7 @@
-import 'package:amoungirl/pages/end_game_page.dart';
 import 'package:amoungirl/pages/roles_allocation.dart';
-import 'package:amoungirl/pages/task_page.dart';
-import 'package:amoungirl/pages/vote_page.dart';
-import 'package:amoungirl/widgets/text_field_decoration.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import "package:socket_io_client/socket_io_client.dart" as IO;
 
 class GameConfigPage extends StatefulWidget {
   static const routeName = 'game_config';
@@ -14,35 +11,26 @@ class GameConfigPage extends StatefulWidget {
 }
 
 class GameConfigPageState extends State<GameConfigPage> {
-  TextEditingController room1Controller = TextEditingController();
-  TextEditingController room2Controller = TextEditingController();
-  TextEditingController room3Controller = TextEditingController();
-  TextEditingController room4Controller = TextEditingController();
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
-  TextEditingController player1Controller = TextEditingController();
-  TextEditingController player2Controller = TextEditingController();
-  TextEditingController player3Controller = TextEditingController();
-  TextEditingController player4Controller = TextEditingController();
+  bool allSelected = false;
 
-  final GlobalKey<FormState> _formKeyRoom = GlobalKey<FormState>();
-  final GlobalKey<FormState> _formKeyPlayer = GlobalKey<FormState>();
+  bool choosePlayerDisabled = false;
+
+  late IO.Socket socket;
+  List<dynamic> players = [];
+
+  String choosenPlayer = "";
 
   @override
   void initState() {
+    initializeSocket();
+
     super.initState();
   }
 
   @override
   void dispose() {
-    room1Controller.dispose();
-    room2Controller.dispose();
-    room3Controller.dispose();
-    room4Controller.dispose();
-    player1Controller.dispose();
-    player2Controller.dispose();
-    player4Controller.dispose();
-    player4Controller.dispose();
-
     super.dispose();
   }
 
@@ -50,134 +38,105 @@ class GameConfigPageState extends State<GameConfigPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Création de jeu"),
+        title: const Text("AMOUNG IRL"),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                roomCreation(),
-                playerCreation(),
-                ElevatedButton(
-                  onPressed: () {
-                    //todo configurer game
-                    print("configurer game");
-                    getAllRoom();
-                    getAllPlayer();
-                  },
-                  child: Text('Configurer game'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    //todo start game
-                    print("start game");
-                    Navigator.of(context).pushNamed(RoleAllocationPage.routeName);
-
-                  },
-                  child: Text("Start"),
-                ),
-              ],
-            )),
+      body: Center(
+        child: Column(
+          children: [
+            buildRadioPlayers(),
+            ElevatedButton(
+              onPressed: choosePlayerDisabled ? null : () => choosePseudo(),
+              child: Text('Choisir ce perso'),
+            ),
+            ElevatedButton(
+                child: Text("START GAME"),
+                onPressed: allSelected ? () => start() : null),
+          ],
+        ),
       ),
     );
   }
 
-  Widget roomCreation() {
-    return Row(
+  void initializeSocket() {
+    socket = IO.io("http://10.57.29.158:3000",
+        IO.OptionBuilder().setTransports(['websocket']).build());
+    socket.connect();
+    print("socket connect ${socket.connected}");
+
+    socket.on('connect', (data) {
+      socket.emit('initGame');
+    });
+
+    socket.on('initGame', (data) {
+      setState(() {
+        players = data['players'];
+      });
+      print("liste de players $players");
+    });
+
+    socket.on('startGame', (data) {});
+
+    socket.on('selectPlayer', (data) {
+      print("ici");
+
+      List dataPlayers = data['players'];
+      setState(() {
+        players = data['players'];
+        allSelected = dataPlayers.every((player) => player['selected']);
+        print('all selcted = $allSelected');
+      });
+    });
+  }
+
+  Widget buildRadioPlayers() {
+    List<Widget> playersRadio = [];
+    for (final player in players) {
+      if (!player['selected']) {
+        Widget r = Radio(
+          visualDensity: VisualDensity.compact,
+          value: player['name'],
+          groupValue: choosenPlayer,
+          onChanged: (value) {
+            setState(() {
+              choosenPlayer = value as String;
+            });
+            print("choosen player = $choosenPlayer");
+          },
+        );
+        Widget t = Text(player['name']);
+        playersRadio.add(r);
+        playersRadio.add(t);
+      }
+    }
+    return Column(
       children: [
-        Form(
-          key: _formKeyRoom,
-          child: Expanded(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 12.0),
-                  child: Text('Entrez les salles de jeux'),
-                ),
-                TextFormField(
-                  controller: room1Controller,
-                  decoration: kTextFieldDecoration.copyWith(
-                      hintText: "Salon", labelText: "Salle 1"),
-                ),
-                TextFormField(
-                  controller: room2Controller,
-                  decoration: kTextFieldDecoration.copyWith(
-                      hintText: "Cuisine", labelText: "Salle 2"),
-                ),
-                TextFormField(
-                  controller: room3Controller,
-                  decoration: kTextFieldDecoration.copyWith(
-                      hintText: "Salle à manger", labelText: "Salle 3"),
-                ),
-                TextFormField(
-                  controller: room4Controller,
-                  decoration: kTextFieldDecoration.copyWith(
-                      hintText: "Grenier", labelText: "Salle 4"),
-                ),
-              ],
-            ),
-          ),
+        Row(
+          children: playersRadio,
         ),
       ],
     );
   }
 
-  Widget playerCreation() {
-    return Row(
-      children: [
-        Form(
-          key: _formKeyPlayer,
-          child: Expanded(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 12.0),
-                  child: Text('Entrez les joueurs'),
-                ),
-                TextFormField(
-                  controller: player1Controller,
-                  decoration:
-                      kTextFieldDecoration.copyWith(labelText: "Joueur 1"),
-                ),
-                TextFormField(
-                  controller: player2Controller,
-                  decoration:
-                      kTextFieldDecoration.copyWith(labelText: "Joueur 2"),
-                ),
-                TextFormField(
-                  controller: player3Controller,
-                  decoration:
-                      kTextFieldDecoration.copyWith(labelText: "Joueur 3"),
-                ),
-                TextFormField(
-                  controller: player4Controller,
-                  decoration:
-                      kTextFieldDecoration.copyWith(labelText: "Joueur 4"),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
-    );
+  Future savePlayerInStorage() async {
+    final SharedPreferences prefs = await _prefs;
+    await prefs.setString("player", choosenPlayer);
+
+    setState(() {
+      choosePlayerDisabled = true;
+    });
   }
 
-  List<String> getAllRoom() {
-    return [
-      room1Controller.text,
-      room2Controller.text,
-      room3Controller.text,
-      room4Controller.text,
-    ];
+  choosePseudo() {
+    print("choisir ce pseudo = ");
+    if (choosenPlayer.isNotEmpty) {
+      socket.emit('selectPlayer', {'name': choosenPlayer});
+      savePlayerInStorage();
+    }
   }
-  List<String> getAllPlayer() {
-    return [
-      player1Controller.text,
-      player2Controller.text,
-      player3Controller.text,
-      player4Controller.text,
-    ];
+
+  start() {
+    print("press");
+    socket.emit('startGame');
+    Navigator.of(context).pushNamed(RoleAllocationPage.routeName);
   }
 }
