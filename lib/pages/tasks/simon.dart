6457f11643a100a2e01b3eabad098f85b705f7c4
DebailleuxJonathan <jonathan.debailleux@edu.itescia.fr>
2022-10-29 19:45:54 +0200

    simon task done

--- lib/pages/tasks/simon.dart
+++ lib/pages/tasks/simon.dart
@@ -22,6 +22,8 @@ class SimonState extends State<Simon> {
   late Timer _timer;
   int _start = 10;
 
+  String scoreSimon = '';
+
   @override
   void initState() {
     startTask();
@@ -45,21 +47,36 @@ class SimonState extends State<Simon> {
           children: [
             Text("Temps restant : $_start"),
             Text("Veuillez jouer au simon"),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Votre score"),
+                  Text(
+                    scoreSimon,
+                    style: TextStyle(fontSize: 50),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
-
       ),
     );
   }
 
   void startTask() {
     startTimer();
+    socketIoClient.socket.on("scoreSimon", (data) {
+      scoreSimon = data;
+    });
+
     socketIoClient.socket.emit(
       "startTask",
       {'task': widget.task, "player": widget.currentPlayer},
     );
   }
-
 
   void startTimer() {
     const oneSec = Duration(seconds: 1);
